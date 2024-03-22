2a4eaf60a792cb90c10ab841ab5596de01a45278
Marcell <marcell.dechant@neuefische.de>
2024-03-22 09:40:50 +0100

    BIG BANG

---
+++ src/main/java/de/neuefische/cachedemo/CacheDemoApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.cachedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CacheDemoApplication.class, args);
+    }
+
+}
